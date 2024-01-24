e034b4d299fcee1ab6ee8b4b8ce9a4c5855419f6
Victoria-94 <146044863+Victoria-94@users.noreply.github.com>
2024-01-23 21:26:19 -0500

    Initial commit

---
+++ proyectoclase/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { mainModule } from 'process';
+import { MainComponent } from './components/main/main.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'proyectoclase';
+}
